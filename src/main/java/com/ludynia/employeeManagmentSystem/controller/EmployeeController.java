5a63d44c56618d7d074f04eb427132a836416c07
Piotr Ludynia <pietrekpl18@gmail.com>
2021-02-14 12:30:40 +0100

    add class for EmployeeController and Interface EmployeeRepository

---
+++ src/main/java/com/ludynia/employeeManagmentSystem/controller/EmployeeController.java
@@ -0,0 +1,9 @@
+package com.ludynia.employeeManagmentSystem.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+}
---
+++ src/main/java/com/ludynia/employeeManagmentSystem/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.ludynia.employeeManagmentSystem.repository;
+
+
+import com.ludynia.employeeManagmentSystem.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    
+}
