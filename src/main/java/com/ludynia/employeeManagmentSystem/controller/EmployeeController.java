e5ac702dd0eef6d8aa0d23cb2c3a82272b72787f
Piotr Ludynia <pietrekpl18@gmail.com>
2021-02-15 21:05:29 +0100

    add sample view to check is controller ok

--- src/main/java/com/ludynia/employeeManagmentSystem/controller/EmployeeController.java
+++ src/main/java/com/ludynia/employeeManagmentSystem/controller/EmployeeController.java
@@ -8,4 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class EmployeeController {
 
+
+
+    public String hello(){
+        return "index";
+    }
+
+
 }
