40d7c5581b387121ceb39cd84746af476643c301
Piotr Ludynia <pietrekpl18@gmail.com>
2021-02-22 12:21:58 +0100

    add method to get all employees

--- src/main/java/com/ludynia/employeeManagmentSystem/controller/EmployeeController.java
+++ src/main/java/com/ludynia/employeeManagmentSystem/controller/EmployeeController.java
@@ -1,17 +1,36 @@
 package com.ludynia.employeeManagmentSystem.controller;
 
 
+import com.ludynia.employeeManagmentSystem.model.Employee;
+import com.ludynia.employeeManagmentSystem.service.EmployeeService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/")
 public class EmployeeController {
 
 
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
 
     public String hello(){
         return "index";
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        List<Employee> list = employeeService.findAll();
+        return new ResponseEntity<List<Employee>>(list,new HttpHeaders(), HttpStatus.OK);
     }
 
 
