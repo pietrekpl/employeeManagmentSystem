9938386fa64b808fc7c7e46417ee2e2a622b705e
Piotr Ludynia <pietrekpl18@gmail.com>
2021-05-03 16:08:55 +0200

    add basic login with security

--- src/main/java/com/ludynia/employeeManagmentSystem/config/WebSecurity.java
+++ src/main/java/com/ludynia/employeeManagmentSystem/config/WebSecurity.java
@@ -1,10 +1,20 @@
 package com.ludynia.employeeManagmentSystem.config;
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
-    // to do configuration methods
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
 }
